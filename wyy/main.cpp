66f30562e086ad50340c260eb483d7d5faa3937b
uniqueFranky <y_r_b@qq.com>
2021-10-20 10:34:21 +0800

    Initial Commit

---
+++ wyy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  wyy
+//
+//  Created by 闫润邦 on 2021/10/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
