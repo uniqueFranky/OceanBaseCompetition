8c28a60d739084fe736e7a8d581c05ee9181613d
uniqueFranky <y_r_b@qq.com>
2021-11-01 21:19:19 +0800

    try to update

--- src/observer/storage/common/table.cpp
+++ src/observer/storage/common/table.cpp
@@ -586,12 +586,13 @@ RC Table::update_record(Trx *trx, const char *attribute_name, const Value *value
     {
         Record tmp = rcd;
         memcpy(tmp.data + offset, value->data, table_meta_.field(attribute_name)->len());
-        /*rc = */delete_record(trx, &rcd);
-//        if(RC::SUCCESS != rc)
-//            return rc;
-        /*rc = */insert_record(trx, &tmp);
-//        if(RC::SUCCESS != rc)
-//            return rc;
+//                rc = delete_entry_of_indexes(rcd.data, rcd.rid, false);
+//                if(RC::SUCCESS != rc)
+//                    return rc;
+                rc = record_handler_->update_record(&tmp);
+                if(RC::SUCCESS != rc)
+                    return rc;
+//                rc = insert_entry_of_indexes(tmp.data, tmp.rid);
     }
     return rc;
 }
